9f72e9e3c555842321da316b51b54ecaeb65727a
xnzar <mustafacetin2043@gmail.com>
2024-04-01 17:19:45 +0300

    models-dto-repository-add

---
+++ src/main/java/com/mustafa/web/models/Club.java
@@ -0,0 +1,29 @@
+package com.mustafa.web.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "clubs")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime createOn;
+    @UpdateTimestamp
+    private LocalDateTime updateOn;
+}
